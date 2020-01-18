4a9f3888d6d09d69f48aa324ee8788d79da92f03
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-18 10:02:47 +0000

    Automatically backed up by Learn

--- src/Components/TVShow.js
+++ src/Components/TVShow.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const tvShow = () => {
+const TvShow = (props) => {
   return (
     <div>
       <br/>
-      <img src={props.image.medium} onClick={props.selectShow} alt=""/>
+      <img src={props.show.image.medium} onClick={()=>props.selectShow(props.show)} alt=""/>
     </div>
   );
 }
 
-export default tvShow;
+export default TvShow;
