ef6da916794b8b002b0f1d84964be3242da12e54
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-18 10:00:40 +0000

    Automatically backed up by Learn

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-const Episode = (props) => {
-  let  myEpisode  = props.eachEpisode
-
+const Filter = (props) => {
   return (
     <div>
-      Episode {myEpisode.number} - {myEpisode.name}
+    <h4> Filter By Rating </h4>
+    <select placeholder="Filter by average rating" onChange={event=>props.handleFilter(event)}>
+      <option>No Filter</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+    </select>
     </div>
   )
 }
-
-export default Episode;
+export default Filter
